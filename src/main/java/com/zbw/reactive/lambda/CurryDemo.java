2a82855aeedead2a32c2b10dbc5eb88c3a8b162d
zhoubinbin <531500726@qq.com>
2021-10-30 22:44:33 +0800

    Lambda 级联表达式和流基础概念

--- src/main/java/com/zbw/reactive/lambda/CurryDemo.java
+++ src/main/java/com/zbw/reactive/lambda/CurryDemo.java
@@ -5,10 +5,13 @@ import java.util.function.Function;
 /**
  * lambda表达式
  * 级联表达式与柯里化
+ * 柯里化：把多个参数的函数转化为只有一个参数的函数
+ * 柯里化的目标：函数标准化
  */
 public class CurryDemo {
     public static void main(String[] args) {
-        //级联表达式
+        //实现了x+y的级联表达式
         Function<Integer, Function<Integer,Integer>> func = x ->y -> x + y;
+        System.out.println(func.apply(2).apply(3));
     }
 }
